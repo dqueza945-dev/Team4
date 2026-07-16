8741e8655e5d2ca7750b9270fe2c1ce4d72a3bca
David Quezada <dqueza945@gmail.com>
2026-07-15 23:41:55 -0700

    Add database class with users/tasks tables, window title, db status label

--- StudyStack/database.h
+++ StudyStack/database.h
@@ -1,10 +1,16 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <QSqlDatabase>
+
 class Database
 {
 public:
     Database();
+    bool init();
+
+private:
+    QSqlDatabase m_db;
 };
 
 #endif // DATABASE_H
--- StudyStack/main.cpp
+++ StudyStack/main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 
+#include "database.h"
+
 #include <QApplication>
+
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication a(argc, argv);  // Start the Qt
+
+    Database db;
+    db.init();   // set up SQLite database
+
     MainWindow w;
-    w.show();
+    w.setWindowTitle("StudyStack");
+    w.show();   // show the main window
     return QApplication::exec();
+
 }
