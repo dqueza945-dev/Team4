b07573016f088f59429fa7221e88f57c3fcb3c8c
Matthew Castulo <castu001@csusm.edu>
2026-07-20 01:13:52 -0700

    Was messing around to learn more about QT creater and added an add task and created a home page.

--- StudyStack/mainwindow.h
+++ StudyStack/mainwindow.h
@@ -2,9 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSqlDatabase>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
+namespace Ui
+{
 class MainWindow;
 }
 QT_END_NAMESPACE
@@ -17,7 +20,36 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
+private slots:
+    // Saves a new task to the database.
+    void addTask();
+
+    // Changes the stacked widget to the Home page.
+    void showHomePage();
+
+    // Changes the stacked widget to the Add Task page.
+    void showAddTaskPage();
+
 private:
     Ui::MainWindow *ui;
+
+    // Connection to the SQLite database.
+    QSqlDatabase database;
+
+    // Opens the database and creates the tasks table.
+    bool initializeDatabase();
+
+    // Loads all saved tasks into the table.
+    void loadTasks();
+
+    // Adds one task to the table displayed on the Home page.
+    void addTaskToTable(
+        const QString &name,
+        const QString &className,
+        const QString &dueDate,
+        double gradeWeight,
+        double estimatedHours
+        );
 };
+
 #endif // MAINWINDOW_H
